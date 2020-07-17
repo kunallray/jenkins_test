b8a1389d3ce51f37e805a31ba1cbb5e8032fad44
kunallray <kunallray@gmail.com>
2020-07-18 04:11:10 +0530

    adding two files

---
+++ demo.py
@@ -0,0 +1,13 @@
+class person:
+    def display(self):
+        print("i am a person")
+
+    def greet(self):
+        print("hello, how are you doing")
+
+p1=person()
+p2=person()
+p1.display()
+p1.greet()
+p2.display()
+p2.greet()
